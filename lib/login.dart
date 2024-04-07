4d5d3ec6baf1b2bef7565fa68c47d43ee4f4d12f
Javi <javi2004.jpl@gmail.com>
2024-04-07 19:42:46 +0200

    Cuerpo del Login realizado
    Acabar:
    - Conectarlo a Firebase
    - Arreglar los 'ifs' de cada TextFormField

--- lib/login.dart
+++ lib/login.dart
@@ -1,88 +1,173 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
-void main() {
+class LoginPage extends StatelessWidget {
 
+  // VARIABLES
+  String? user = '';
   String? email = '';
-  String? password = '';
+  String? password1 = '';
+  String? password2 = '';
 
   String emailCorrecto = 'javi@gmail.com';
-  String passwordCorrecto = 'Javier';
+  String passwordCorrecto = 'javier2004';
 
-  runApp(MaterialApp(
-    home: Scaffold(
+  // METODOS
+  void iniciarSesion() {
+  }
 
+
+  // MAIN
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Inicio de Sesion'),
+        title: Text('INICIO DE SESIÓN'),
+        backgroundColor: Colors.lightBlue
       ),
 
       body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: ListView(
-          children: <Widget>[
-            Card(
-              margin: EdgeInsets.all(5.0),
-              elevation: 4.0,
-              child: Container(
-                margin: EdgeInsets.all(16.0),
-                child: Form(
-                  child: Column(
-                    children: <Widget>[
-                      TextFormField(
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          hintText: 'Introduzca su email',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0)
-                          )
-                        ),
-                        validator: ( String? value ) {
-                          if( value!.isEmpty ) {
-                            return 'El campo esta vacio';
-                          }
-                          else {
-                            if( value.contains(' ') ) {
-                              return 'El email contiene espacios';
+          padding: EdgeInsets.all(16.100),
+          child: ListView(
+            children: <Widget>[
+              Card(
+                margin: EdgeInsets.all(5.0),
+                elevation: 4.0,
+                child: Container(
+                  margin: EdgeInsets.all(16.0),
+                  child: Form(
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Nombre del usuario',
+                              hintText: 'Introduzca su contraseña',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0)
+                              )
+                          ),
+                          validator: ( String? value ) {
+                            if( value!.isEmpty ) {
+                              return 'El campo esta vacio';
                             }
                             else {
-                              return null;
+                              if( value.length < 6 ) {
+                                return 'Contraseña demasiado corta';
+                              }
+                              else {
+                                return null;
+                              }
                             }
-                          }
-                        },
-                        onSaved: ( String? value ) => email = value,
-                      ),
-                      TextFormField(
-                        decoration: InputDecoration(
-                            labelText: 'Contraseña',
-                            hintText: 'Introduzca su contraseña',
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5.0)
-                            )
+                          },
+                          onSaved: ( String? value ) => user = value,
                         ),
-                        validator: ( String? value ) {
-                          if( value!.isEmpty ) {
-                            return 'El campo esta vacio';
-                          }
-                          else {
-                            if( value.length < 6 ) {
-                              return 'Contraseña demasiado corta';
+                        SizedBox(height: 15),
+
+                        TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Email',
+                              hintText: 'Introduzca su email',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0)
+                              )
+                          ),
+                          validator: ( String? value ) {
+                            if( value!.isEmpty ) {
+                              return 'El campo esta vacio';
                             }
                             else {
-                              return null;
+                              if( value.contains(' ') ) {
+                                return 'El email contiene espacios';
+                              }
+                              else {
+                                return null;
+                              }
                             }
-                          }
-                        },
-                        onSaved: ( String? value ) => email = value,
-                      )
-                    ],
+                          },
+                          onSaved: ( String? value ) => email = value,
+                        ),
+                        SizedBox(height: 15),
+
+                        TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Contraseña',
+                              hintText: 'Introduzca su contraseña',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0)
+                              )
+                          ),
+                          validator: ( String? value ) {
+                            if( value!.isEmpty ) {
+                              return 'El campo esta vacio';
+                            }
+                            else {
+                              if( value.length < 6 ) {
+                                return 'Contraseña demasiado corta';
+                              }
+                              else {
+                                return null;
+                              }
+                            }
+                          },
+                          onSaved: ( String? value ) => password1 = value,
+                        ),
+                        SizedBox(height: 15),
+
+                        TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Confirmar contraseña',
+                              hintText: 'Introduzca su contraseña',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0)
+                              )
+                          ),
+                          validator: ( String? value ) {
+                            if( value!.isEmpty ) {
+                              return 'El campo esta vacio';
+                            }
+                            else {
+                              if( value.length < 6 ) {
+                                return 'Contraseña demasiado corta';
+                              }
+                              else {
+                                return null;
+                              }
+                            }
+                          },
+                          onSaved: ( String? value ) => password2 = value,
+                        ),
+                        SizedBox(height: 15),
+
+                        Row(
+                          children: <Widget>[
+                            Checkbox(value: false, onChanged: null),
+                            Text('Recordar contraseña')
+                          ],
+                        ),
+                        SizedBox(height: 235),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            ElevatedButton(
+                              child: Text('Iniciar Sesión', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+                              onPressed: () => iniciarSesion(),
+                            ),
+                          ],
+
+                        )
+
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            )
+              )
 
-          ],
-        )
+            ],
+          )
       ),
-    )
-  ));
 
+    );
+  }
 }
+
