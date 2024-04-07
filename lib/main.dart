3232c49950f04056d6b8044e7b03565d39bbc118
JWinn01 <joshua.winn.2001@gmail.com>
2024-04-07 17:59:00 +0200

    New classes

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,27 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Otaku Tracing'),
+        title: Text(
+            'Otaku Tracing',
+                style: TextStyle(
+                fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                  color: Colors.white
+                ),
+        ),
         centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Text('Bienvenido'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
-        child: Text('click'),
+        child: Text(
+            'click'
+        ),
+        backgroundColor: Colors.black,
       ),
     )
   ));
