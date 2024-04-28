6c37b55cfa65e5f4c16b73067e42c53663f5daf9
JWinn01 <joshua.winn.2001@gmail.com>
2024-04-28 17:03:14 +0200

    correcciones del mediaPlayer y comienzo de inserccion de graphql

---
+++ lib/GrahpqlData.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+class GraphqlData {
+  final HttpLink httpLink = HttpLink('https://your-graphql-api-url.com/graphql');
+
+}
+
